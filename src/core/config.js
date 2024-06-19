6a526f9b7d5721e15cf571ddbb1aa038291ad40f
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-06-19 19:22:16 +0530

    Config Commit

--- src/core/config.js
+++ src/core/config.js
@@ -1,3 +1,27 @@
-const config = {};
+const config = {
+	subjects: [
+		{
+			subjectName: 'Science',
+			passedCount: 83,
+		},
+		{
+			subjectName: 'English',
+			passedCount: 60,
+		},
+		{
+			subjectName: 'History',
+			passedCount: 91,
+		},
+		{
+			subjectName: 'Maths',
+			passedCount: 43,
+		},
+		{
+			subjectName: 'Music',
+			passedCount: 100,
+		},
+	],
+
+};
 
 export default config;
