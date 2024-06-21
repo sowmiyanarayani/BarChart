2726bfed200ba59afb42a01958f4d891305e617a
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-06-21 11:51:49 +0530

    Data Commit

---
+++ src/Data/StudentData.js
@@ -0,0 +1,24 @@
+const subjects = [
+	{
+		subjectName: 'Science',
+		passedCount: 83,
+	},
+	{
+		subjectName: 'English',
+		passedCount: 60,
+	},
+	{
+		subjectName: 'History',
+		passedCount: 91,
+	},
+	{
+		subjectName: 'Maths',
+		passedCount: 43,
+	},
+	{
+		subjectName: 'Music',
+		passedCount: 100,
+	},
+];
+
+export default subjects;
