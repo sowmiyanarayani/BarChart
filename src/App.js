7273d955d55f8331c6603aaebdd5487a8acec60f
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-06-24 11:40:52 +0530

    Refactored Commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import BarGraph from './components/BarGraph';
 const App = (context) =>
 	<div className="App">
+		<h2>Passed Count</h2>
 		<BarGraph { ...context }/>
 	</div>;
 
